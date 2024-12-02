8137e6e0c1ad9f58ac4f86797bc79f74dfb6fb2c
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-12-02 16:12:56 -0600

    Add project files.

---
+++ Sydneys-Strung-Creations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sydneys_Strung_Creations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
