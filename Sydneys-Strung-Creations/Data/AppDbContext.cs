389a1cf97d08afd8c7db1070cd8751d75834d9bc
Savannah Stephenson <stephensonsavannah@ymail.com>
2024-12-02 21:52:37 -0600

    fixing some database issues

--- Sydneys-Strung-Creations/Data/AppDbContext.cs
+++ Sydneys-Strung-Creations/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Sydneys_Strung_Creations.Models;
 
 namespace Sydneys_Strung_Creations.Data
 {
@@ -6,7 +7,8 @@ namespace Sydneys_Strung_Creations.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
         }
+        public DbSet<gallary_listing> gallary_listing { get; set; }
+        public DbSet<jewlery_template> jewlery_template { get; set; }
     }
 }
--- Sydneys-Strung-Creations/Program.cs
+++ Sydneys-Strung-Creations/Program.cs
@@ -1,7 +1,12 @@
+using Sydneys_Strung_Creations.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
