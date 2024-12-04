8a44e5e21e2f9f91350c4eafb3a363654a136afc
cakewaterrr <cduff4@islander.tamucc.edu>
2024-12-03 21:46:04 -0600

    adding the Contact Us Page

--- Sydneys-Strung-Creations/Program.cs
+++ Sydneys-Strung-Creations/Program.cs
@@ -26,9 +26,16 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//   name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 // Seed the database
 AppDbInitalizer.Seed(app);
